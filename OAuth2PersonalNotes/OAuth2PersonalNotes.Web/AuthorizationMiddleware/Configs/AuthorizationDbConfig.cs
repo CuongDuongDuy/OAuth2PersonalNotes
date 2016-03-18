37ae9961be4dceea8dcda9cf60ce6f2a9f7849ac
CuongDuongDuy <cuong.duongduy@gmail.com>
2016-03-18 11:13:33 +0000

    #Apply OWIN Middleware for Authorization.

---
+++ OAuth2PersonalNotes/OAuth2PersonalNotes.Web/AuthorizationMiddleware/Configs/AuthorizationDbConfig.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+using OAuth2PersonalNotes.Web.AuthorizationMiddleware.Models;
+
+namespace OAuth2PersonalNotes.Web.AuthorizationMiddleware.Configs
+{
+    public class AuthorizationDbInitializer : DropCreateDatabaseIfModelChanges<AuthorizationDbContext>
+    {
+    }
+}
